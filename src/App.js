1cbeb75bb740a0d1b262c14cfea9af237b384b93
ZarmanRU <kampakcotsava@gmail.com>
2023-12-19 18:46:22 +0300

    f2o

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Борисенко С.Э.";
 
-  console.log('This is App ');
+ 
   return (
     <div className="App">
      <header className="App-header">
